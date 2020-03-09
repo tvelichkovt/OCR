9e5799d29bd32cdef19800f38f77ce4d4840941a
Todor <tvelichkovt@gmail.com>
2020-03-09 15:13:03 +0200

    Update OCR _From_PDFs.py

--- OCR _From_PDFs.py
+++ OCR _From_PDFs.py
@@ -17,20 +17,20 @@ for page in pages:
     page.save(filename, 'JPEG') 
     image_counter = image_counter + 1
 
-# 3. Save txt file
+# 3. Save txt file + encoding="utf-8" for Bulgarian /remove for English/
     
 filelimit = image_counter-1
 outfile = "OCR _From_Pdf.txt"
 
-f = open(outfile, "a", encoding="utf-8")
+f = open(outfile, "a", encoding="utf-8")#, encoding="utf-8") for Bulgarian
 
-# 4 Iterate from 1 to total number of pages 
+# 4 Iterate from 1 to total number of pages, Tesseract up + lang="bul"
 
 pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe' #, encoding="utf-8") for Bulgarian
 
 for i in range(1, filelimit + 1): 
     filename = "page_"+str(i)+".jpg"
-    text = str(((pytesseract.image_to_string(Image.open(filename), lang="bul")))) #, , lang="bul") for Bulgarian
+    text = str(((pytesseract.image_to_string(Image.open(filename), lang="bul")))) #, lang="bul") for Bulgarian
     text = text.replace('-\n', '')     #replace every '-\n' to ''
     f.write(text) 
   
